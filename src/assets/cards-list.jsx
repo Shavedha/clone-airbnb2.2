e3ba59fc4a2500d19bc305378935c69fe9156b11
Shavedha <vijiharsha1531@gmail.com>
2024-06-24 20:39:28 +0530

    updated

--- src/assets/cards-list.jsx
+++ src/assets/cards-list.jsx
@@ -9,10 +9,10 @@ export const list = [
         ], 
         price: "11000", 
         date: "24-28June", 
-        title: "ABC",
+        title: "Jharkhand,India",
         desc:"New home",
         rating:"4.1",
-        category: "Icons",
+        category: "City Hoouse",
     },
 
     {
@@ -20,16 +20,115 @@ export const list = [
             "https://i.pinimg.com/474x/9b/a9/29/9ba929a05455f5ca01d217a6c11ae5ff.jpg",
             "https://i.pinimg.com/474x/34/8e/a5/348ea5b2f6f322004c4e78c9bffef8d6.jpg",
             "https://i.pinimg.com/474x/ad/6a/06/ad6a06ff56df3dda969f381219a0d256.jpg",
-            "https://i.pinimg.com/474x/b7/c8/e8/b7c8e8ae965ffe4bf9e996d79bf6f31b.jpg",
-            "https://i.pinimg.com/474x/4c/e6/12/4ce612236ed83fa8ea9733f2c5d14a5c.jpg",
         ], 
         price: "₹4700", 
         date: "1-4 Sep", 
         title: "TreeCamps",
+        desc:"Camps",
+        rating:"4.6",
+        category: "Tree House",
+        
+    },
+
+    {
+        imgSrc:[
+            
+            "https://i.pinimg.com/474x/b7/c8/e8/b7c8e8ae965ffe4bf9e996d79bf6f31b.jpg",
+            "https://i.pinimg.com/474x/4c/e6/12/4ce612236ed83fa8ea9733f2c5d14a5c.jpg",
+        ], 
+        price: "5600", 
+        date: "1-4 Sep", 
+        title: "TreeCamps",
+        desc:"Amazon",
+        rating:"4.6",
+        category: "Tree House",
+        
+    },
+    {
+        imgSrc:[
+           "https://i.pinimg.com/474x/a0/91/13/a09113a57cec5e9233409e3c26de67d1.jpg",
+           "https://i.pinimg.com/474x/9c/e4/69/9ce46963f8c67a8cefe278b3417e74cd.jpg",
+        ], 
+        price: "₹6500" ,
+        date: "14-18 Sep", 
+        title: "TreeCamps",
         desc:"Meddenahali,India",
         rating:"4.6",
-        category: "Arctic",
+        category: "Tree House",
         
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/736x/48/c3/7c/48c37c6efd630274c82f341e0352fe42.jpg",
+            "https://assets.cntraveller.in/photos/61b345111c6a75b8687e565f/master/w_1600%2Cc_limit/Yarlam.jpeg",
+            "https://th.bing.com/th/id/OIP.CDUn79VxXVoVM68Ko3sDOwAAAA?pid=ImgDet&w=178&h=133&c=7&dpr=1.5",
+        ],
+        price: "₹4700",
+        date: "14-17th Sep",
+        title: "Arctic",
+        desc: "Sikkim,India",
+        rating: "4.78",
+        category: "Arctic",
+    },
+
+    {
+        imgSrc:[
+            "https://media.cntraveler.com/photos/5c253feebc3f676aee45ecea/4:3/w_480,c_limit/arctic-hideaway-cr-Pasi-Aalto.jpg",
+            "https://th.bing.com/th/id/OIP.PxWvy6kbcfkQBUA8kNXwxwHaE7?pid=ImgDet&w=178&h=118&c=7&dpr=1.5",
+            "https://th.bing.com/th/id/OIP.t66meRPekd_nvSKlWKLT_AHaE4?pid=ImgDet&w=178&h=117&c=7&dpr=1.5",
+        ],
+        price: "₹7000",
+        date: "20-24th Sep",
+        title: "Arctic",
+        desc: "Sikkim,India",
+        rating: "4.56",
+        category: "Arctic",
+    },
+
+    {
+        imgSrc:[
+            
+            "https://i.pinimg.com/474x/09/8a/4b/098a4b9e3e25516040a2a3c5ce93b02d.jpg",
+            "https://i.pinimg.com/474x/5f/b0/dc/5fb0dca18b6f9e920f491069b3d06d46.jpg",
+        ], 
+        price: "₹8000", 
+        date: "4-10 Aug", 
+        title: "Caravan",
+        desc:"Scoland",
+        rating:"4.2",
+        category: "Caravans",
+
+    },
+
+    {
+        imgSrc:[
+            
+           "https://i.pinimg.com/474x/6b/ed/82/6bed824cbbded2662d212084e992a72d.jpg",
+           "https://i.pinimg.com/474x/3a/dc/f9/3adcf9169ecaba507b44785f7a800859.jpg",
+        ], 
+        price: "₹8000", 
+        date: "7-10 Aug", 
+        title: "Caravan",
+        desc:"Scoland,UK",
+        rating:"4.2",
+        category: "Caravans",
+
+    },
+
+    {
+        imgSrc:[
+            
+            "https://i.pinimg.com/474x/34/1c/73/341c73ad7f70955a0b038f227a01b4fc.jpg",
+            "https://i.pinimg.com/474x/f3/bb/7c/f3bb7c5790e27b7b58c7fe4f54d3f978.jpg",
+        ], 
+        price: "₹2000", 
+        date: "8-10 Aug", 
+        title: "Caravan",
+        desc:"Scoland",
+        rating:"4.2",
+        category: "Caravans",
+
     },
 
     {
@@ -37,18 +136,16 @@ export const list = [
             "https://i.pinimg.com/474x/00/1f/c2/001fc2cb3f7d6c3da748feeb48829c2d.jpg",
             "https://i.pinimg.com/474x/f8/08/9b/f8089ba9888812cbd8ea92dd8847eed1.jpg",
             "https://i.pinimg.com/474x/cd/6e/4b/cd6e4b783694ef3164559d3008df0345.jpg",
-            "https://i.pinimg.com/474x/09/8a/4b/098a4b9e3e25516040a2a3c5ce93b02d.jpg",
-            "https://i.pinimg.com/474x/5f/b0/dc/5fb0dca18b6f9e920f491069b3d06d46.jpg",
+           
         ], 
-        price: "₹23000", 
-        date: "7-10 Aug", 
+        price: "₹8870", 
+        date: "9-12 Aug", 
         title: "Caravan",
-        desc:"Scoland",
-        rating:"4.2",
-        category: "Castles",
+        desc:"Australia",
+        rating:"4.23",
+        category: "Caravans",
 
     },
-
     {
         imgSrc:[
             "https://i.pinimg.com/474x/64/43/45/6443455c1c9fe85626f9491ac31e3458.jpg",
@@ -60,10 +157,76 @@ export const list = [
         price: "₹34000", 
         date: "24-28th June", 
         title: "Castles",
-        desc:"Bujra,India",
+        desc:"Scotland,UK",
         rating:"4.4",
-        category: "Play",
+        category: "Castles",
     },
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/88/20/55/8820559dcfefbbd4c0fbe3d1029bf73a.jpg",
+            "https://i.pinimg.com/474x/8b/01/ab/8b01ab6c77a0c8c4972c45d22f91af64.jpg",
+         ], 
+        price: "₹32000", 
+        date: "24-28th July", 
+        title: "Castles",
+        desc:"Scotland,UK",
+        rating:"4.24",
+        category: "Castles",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/80/b5/cf/80b5cf7abf0e9d133cbdc9f8106d9d09.jpg",
+            "https://i.pinimg.com/474x/0f/75/de/0f75de0885e73065a909e5e6fbfd46f3.jpg",
+         ], 
+        price: "₹21000", 
+        date: "24-28th Nov", 
+        title: "Castles",
+        desc:"Scotland,UK",
+        rating:"4.34",
+        category: "Castles",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/db/bd/7c/dbbd7c501eae706d10c10855ae0baf78.jpg",
+            "https://i.pinimg.com/474x/7a/37/9a/7a379a69fc133f38eaf937ed3affaabd.jpg",
+         ], 
+        price: "₹4000", 
+        date: "24-28th Nov", 
+        title: "Castles",
+        desc:"Scotland,UK",
+        rating:"4.95",
+        category: "Castles",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/30/f6/99/30f69900aad8061c5496ceaf08093095.jpg",
+            "https://i.pinimg.com/474x/f1/b0/53/f1b053cbc75072ab2c287f2debbe76c9.jpg",
+         ], 
+        price: "₹3200", 
+        date: "2-8th Nov", 
+        title: "Castles",
+        desc:"Scotland,UK",
+        rating:"4.77",
+        category: "Castles",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/e9/f6/81/e9f681d0740066f43568873c914f0839.jpg",
+            "https://i.pinimg.com/474x/fc/46/ad/fc46adfc12967d9fac731b32d40f79a9.jpg",
+         ], 
+        price: "₹3200", 
+        date: "2-8th Nov", 
+        title: "Castles",
+        desc:"Scotland,UK",
+        rating:"4.43",
+        category: "Castles",
+    },
+
+
 
     {
         imgSrc:[
@@ -79,7 +242,35 @@ export const list = [
         title: "Beach houses",
         desc:"Kerala,India",
         rating:"4.9",
-        category: "House Boats",
+        category: "Beach Front",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/a2/70/32/a27032012e3fb1792c73c3433e544b3c.jpg",
+            "https://i.pinimg.com/474x/ce/dd/58/cedd58c8e011c90403d0d9f35ce422a2.jpg",
+            "https://i.pinimg.com/474x/00/1f/bf/001fbf31374dd7a58588c08ca5bfb423.jpg",
+             ], 
+        price: "₹7000", 
+        date: "24-28th June", 
+        title: "Beach houses",
+        desc:"FLorida",
+        rating:"4.94",
+        category: "Beach Front",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/60/c8/07/60c80702eedfe6fa07b27515a5784c87.jpg",
+            "https://i.pinimg.com/474x/2c/b3/3c/2cb33cfa440d7073692825261e88ce0f.jpg",
+            
+             ], 
+        price: "₹7000", 
+        date: "4-7th Sept", 
+        title: "Hotel Tide",
+        desc:"Hotel Tide",
+        rating:"4.4",
+        category: "Beach Front",
     },
 
     {
@@ -92,28 +283,54 @@ export const list = [
              ], 
         price: "₹23000", 
         date: "24-28th June", 
-        title: "Manshion",
-        desc:"NY City",
+        title: "Mumbai,India",
+        desc:"Mansion",
         rating:"4.43",
-        category: "Top cities",
+        category: "City House",
     },
 
     {
         imgSrc:[
             "https://i.pinimg.com/474x/83/07/5b/83075b853eb08bc940d1dad65352825d.jpg",
         "https://i.pinimg.com/474x/5e/7d/da/5e7dda36c5854a1e5826ecf4e7e6a1fd.jpg",
-          "https://i.pinimg.com/474x/41/b2/45/41b2456794e1fb7f35d3b8246656eef6.jpg",
-         "https://i.pinimg.com/474x/24/da/f9/24daf97f04c3c2cf23a701f39584ec28.jpg",
-          "https://i.pinimg.com/474x/ad/d3/bc/add3bc33b9b298d10aebd8f4af46769a.jpg",
-            "https://i.pinimg.com/474x/c3/d0/a8/c3d0a81f0217fd0fc7e5bfac4cc5baab.jpg",
-           "https://i.pinimg.com/474x/be/6a/f5/be6af5df94628eac830fa2580cfa980a.jpg", 
+         
     ], 
         price: "₹18800", 
         date: "24-28th June", 
         title: "Cave Houses",
         desc:"Quepem",
-        rating:"4.69",
-        category: "Tree Houses",
+        rating:"4.4",
+        category: "Caves",
+    },
+
+    {
+        imgSrc:[
+           
+          "https://i.pinimg.com/474x/41/b2/45/41b2456794e1fb7f35d3b8246656eef6.jpg",
+         "https://i.pinimg.com/474x/24/da/f9/24daf97f04c3c2cf23a701f39584ec28.jpg",
+         
+    ], 
+        price: "₹14900", 
+        date: "24-28th June", 
+        title: "Cave Houses",
+        desc:"Quepem",
+        rating:"4.4",
+        category: "Caves",
+    },
+
+    {
+        imgSrc:[
+            
+          "https://i.pinimg.com/474x/ad/d3/bc/add3bc33b9b298d10aebd8f4af46769a.jpg",
+            "https://i.pinimg.com/474x/c3/d0/a8/c3d0a81f0217fd0fc7e5bfac4cc5baab.jpg",
+           "https://i.pinimg.com/474x/be/6a/f5/be6af5df94628eac830fa2580cfa980a.jpg", 
+    ], 
+        price: "₹13000", 
+        date: "24-28th June", 
+        title: "Cave Houses",
+        desc:"Quepem",
+        rating:"4.6",
+        category: "Caves",
     },
 
     {
@@ -124,10 +341,10 @@ export const list = [
               ], 
         price: "₹43000", 
         date: "24-28th June", 
-        title: "H house",
+        title: "Mumbai,India",
         desc:"Dandeghar",
         rating:"4.77",
-        category: "National Parks",
+        category: "City House",
     },
 
     {
@@ -141,14 +358,55 @@ export const list = [
         title: "House Boats",
         desc:"Baga",
         rating:"4.87",
-        category: "Surfing",
+        category: "Boat House",
     },
 
     {
         imgSrc:[
-            "https://i.pinimg.com/474x/cf/01/d3/cf01d3784290fbdfd35e3eb79608b970.jpg",
-            "https://i.pinimg.com/474x/c8/91/ef/c891ef4d86dd84b8bd84b89295322514.jpg",
-            "https://i.pinimg.com/474x/05/16/60/05166035f63fcd3eaa4c2fd46288abea.jpg",
+            "https://i.pinimg.com/474x/e9/22/eb/e922eb7e47c5c72d3a0e86690dd72d14.jpg",
+            "https://i.pinimg.com/474x/65/3f/ed/653fed0f57c9c77a02a710571778ec79.jpg",
+            "https://i.pinimg.com/474x/33/0e/3b/330e3b43b1fc23a88c79e08f95c1433b.jpg",
+             ], 
+        price: "₹5000", 
+        date: "2-8th July", 
+        title: "House Boats",
+        desc:"Kerala,IND",
+        rating:"4.33",
+        category: "Boat House",
+    },
+    
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/b4/11/2c/b4112cc294aa566e857b91a13670aad8.jpg",
+            "https://i.pinimg.com/474x/3d/a2/ee/3da2ee340e22b18887a5f5f60670bc1c.jpg",
+            "https://i.pinimg.com/474x/dd/53/08/dd530846c7d685021b1710b56a96ba8f.jpg",
+             ], 
+        price: "₹4300", 
+        date: "26-20th Oct", 
+        title: "House Boats",
+        desc:"Kerala,IND",
+        rating:"4.45",
+        category: "Boat House",
+    },
+
+    {
+        imgSrc:[
+            "https://th.bing.com/th/id/OIP.9Ir2ryE_kxaKN2CXY_QPEgHaE8?w=300&h=200&c=7&o=5&dpr=1.5&pid=1.7",
+            "https://th.bing.com/th/id/OIP.l2YZXVHeqd451XDCjsNSmQHaDb?w=333&h=161&c=7&r=0&o=5&dpr=1.5&pid=1.7",
+        ], 
+        price: "₹4300", 
+        date: "12-14th Oct", 
+        title: "House Boats",
+        desc:"Kerala,IND",
+        rating:"3.87",
+        category: "Boat House",
+    },
+
+
+    {
+        imgSrc:[
+            // "https://i.pinimg.com/474x/cf/01/d3/cf01d3784290fbdfd35e3eb79608b970.jpg",
+            
             "https://i.pinimg.com/474x/3b/09/8a/3b098a835e97d593b7c8088661bff9cc.jpg",
             "https://i.pinimg.com/474x/5e/62/d6/5e62d69a11ec1c587c083ef5276051d8.jpg",
             ], 
@@ -157,7 +415,34 @@ export const list = [
         title: "Vineyards",
         desc:"Pune",
         rating:"4.85",
-        category: "Beach Front",
+        category: "Vineyards",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/cf/01/d3/cf01d3784290fbdfd35e3eb79608b970.jpg",
+            "https://i.pinimg.com/474x/c8/91/ef/c891ef4d86dd84b8bd84b89295322514.jpg",
+            "https://i.pinimg.com/474x/5e/62/d6/5e62d69a11ec1c587c083ef5276051d8.jpg",
+            ], 
+        price: "₹3000", 
+        date: "24-28th Nov", 
+        title: "Vineyards",
+        desc:"Italy",
+        rating:"4.85",
+        category: "Vineyards",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/29/0c/55/290c553caff49e818eda08ae891c603a.jpg",
+            "https://i.pinimg.com/474x/79/02/81/790281be06a08b615e0e86c8ea7c71f4.jpg",
+            ], 
+        price: "₹3000", 
+        date: "24-28th Nov", 
+        title: "Vineyards",
+        desc:"Pune",
+        rating:"4.85",
+        category: "Vineyards",
     },
 
     {
@@ -166,14 +451,52 @@ export const list = [
             "https://i.pinimg.com/474x/43/15/92/431592fac10d0e39b4c4796605cff2cd.jpg",
             "https://i.pinimg.com/474x/3b/41/b5/3b41b552dfdad680af7b038781374690.jpg",
             ], 
-        price: "₹14000", 
+        price: "₹41000", 
         date: "24-28th June", 
         title: "Deserts",
-        desc:"",
+        desc:"Dubai,UAE",
         rating:"4.18",
-        category: "Mountain House",
+        category: "Deserts",
     },
 
+    {
+        imgSrc:[
+             "https://i.pinimg.com/474x/cb/ee/e8/cbeee8092909892580a3f0c5ca3dd3b7.jpg",
+             "https://i.pinimg.com/474x/2a/4f/44/2a4f4432fc22b3d665b00870e92509f2.jpg",
+            ], 
+        price: "₹6700", 
+        date: "24-28th June", 
+        title: "Deserts",
+        desc:"UAE",
+        rating:"4.1",
+        category: "Deserts",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/ff/68/f6/ff68f62bfec86eeb9c78f72380e3bc07.jpg",
+            "https://i.pinimg.com/474x/7e/a7/52/7ea752d74958005129b1b142b7a078df.jpg",
+        ],
+        price: "₹5600", 
+        date: "24-28th June", 
+        title: "Deserts",
+        desc:"UAE",
+        rating:"4.13",
+        category: "Deserts",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/84/0f/05/840f050e966daf3c21a241470376d273.jpg",
+            "https://i.pinimg.com/474x/1f/6b/6a/1f6b6ad72d6865718c9011712d6edc52.jpg",
+        ],
+        price: "₹7800", 
+        date: "24-28th June", 
+        title: "Deserts",
+        desc:"UAE",
+        rating:"4.78",
+        category: "Deserts",
+    },
     {
         imgSrc:[
             "https://i.pinimg.com/474x/32/c8/54/32c854725d2a99ab821d5c2848dc156a.jpg",
@@ -185,7 +508,7 @@ export const list = [
         title: "Lake house",
         desc:"Tambon Karon",
         rating:"4.56",
-        category: "FarmHouses",
+        category: "Lakefront",
     },
 
     {
@@ -199,7 +522,7 @@ export const list = [
         title: "Wind Mills",
         desc:"Tambon Rawai",
         rating:"4.87",
-        category: "FarmHouses",
+        category: "Lakefront",
     },
 
     {
@@ -211,10 +534,10 @@ export const list = [
              ], 
         price: "₹7800", 
         date: "24-28th June", 
-        title: "Topics",
+        title: "Lakefront",
         desc:"Mananthavady",
         rating:"4.49",
-        category: "Tree Houses",
+        category: "Lakefront",
     },
 
     {
@@ -225,10 +548,107 @@ export const list = [
         ], 
         price: "₹3000", 
         date: "24-28th June", 
-        title: "A-frames",
+        title: "Tropicals",
         desc:"Chavsar",
         rating:"4.59",
-        category:"Castles",
+        category:"National Parks",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/55/50/89/5550895b0517db70f8f7059d5013b6dd.jpg",
+            "https://i.pinimg.com/474x/70/40/2d/70402d094ed53deed0077fcb41b8da95.jpg",
+
+        ],
+        price: "₹13000", 
+        date: "5-9th Sept", 
+        title: "Farm House",
+        desc:"Kerala,IND",
+        rating:"4.6",
+        category: "FarmHouses",
+    },
+
+    {
+        imgSrc:[
+           "https://i.pinimg.com/474x/fa/eb/b6/faebb697afdd8868b59d7a283b78fb28.jpg",
+           "https://i.pinimg.com/474x/1f/36/8b/1f368b3f4c8156aa7ba3d43a85c44d2f.jpg",
+
+        ],
+        price: "₹17800", 
+        date: "12-15th Sept", 
+        title: "Farm House",
+        desc:"Kerala,IND",
+        rating:"4.23",
+        category: "FarmHouses",
+    },
+
+    {
+        imgSrc:[
+           "https://i.pinimg.com/474x/5d/c4/e0/5dc4e0c02ff9cd5e8016bbeaead55707.jpg",
+           "https://i.pinimg.com/474x/df/65/83/df65839f0639f67379ba7f3ce2aee833.jpg",
+
+        ],
+        price: "₹17800", 
+        date: "16-19th July", 
+        title: "Farm House",
+        desc:"Kerala,IND",
+        rating:"4.53",
+        category: "FarmHouses",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/d9/3c/c3/d93cc3af67bda944a25de5f583109595.jpg",
+            "https://i.pinimg.com/474x/25/f0/4b/25f04b40b34330dcba1952661c59f311.jpg",
+
+        ],
+        price: "₹11700", 
+        date: "16-19th July", 
+        title: "Farm House",
+        desc:"Kerala,IND",
+        rating:"4.37",
+        category: "FarmHouses",
+    },
+
+    {
+        imgSrc:[
+          "https://i.pinimg.com/474x/73/5a/4a/735a4a0f48d488b041763aa684815165.jpg",
+          "https://i.pinimg.com/474x/7d/56/ce/7d56ce0b0edd5991852b98bf55214290.jpg",
+
+        ],
+        price: "₹1900", 
+        date: "16-21th July", 
+        title: "Farm House",
+        desc:"Kerala,IND",
+        rating:"4.37",
+        category: "FarmHouses",
+    },
+
+    {
+        imgSrc:[
+          "https://i.pinimg.com/474x/c5/69/d2/c569d2fbb9303b08c1382d6e3a9b36d6.jpg",
+          "https://i.pinimg.com/474x/5e/d2/d6/5ed2d6f060316574f738b0e15e61bda9.jpg",
+
+        ],
+        price: "₹1900", 
+        date: "16-21th July", 
+        title: "Farm House",
+        desc:"Kerala,IND",
+        rating:"4.37",
+        category: "FarmHouses",
+    },
+
+    {
+        imgSrc:[
+            "https://i.pinimg.com/474x/31/8e/22/318e223a69fd4d446d1cd84ddb9b7655.jpg",
+            "https://i.pinimg.com/474x/77/a8/15/77a81544d6ed96dd140bd57318c60af2.jpg",
+        ],
+        price: "₹1900", 
+        date: "16-21th July", 
+        title: "Farm House",
+        desc:"Kerala,IND",
+        rating:"4.37",
+        category: "FarmHouses",
     },
 ];
 
